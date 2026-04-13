8b31e66fc0dda67cc984870dd0c92e812f3d53b5
Eris <eris@erisws.com>
2026-04-13 18:12:03 -0400

    gradlew

--- fusionApp/build.gradle.kts
+++ fusionApp/build.gradle.kts
@@ -46,7 +46,7 @@ android {
     externalNativeBuild {
         cmake {
             path = File("./src/main/jni/CMakeLists.txt")
-            version = "3.31.1"
+            version = "4.3.1"
         }
     }
 
