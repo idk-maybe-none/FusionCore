14398ec1786ec284700f863505cced32e2d6439f
XtraCube <72575280+XtraCube@users.noreply.github.com>
2026-04-13 00:29:29 -0400

    rebuilt dotnet (not tested)

--- fusionApp/build.gradle.kts
+++ fusionApp/build.gradle.kts
@@ -11,7 +11,6 @@ repositories {
 }
 
 dependencies {
-    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     implementation("org.jetbrains.kotlin:kotlin-stdlib:2.3.20")
     implementation("top.canyie.pine:core:0.3.0")
     implementation("io.github.hexhacking:xdl:2.3.0")
